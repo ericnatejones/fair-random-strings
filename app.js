febb0b7b3321e50e81cc903c22c04cf188e63be5
Eric Jonese <ericnatejones@gmail.com>
2017-09-21 00:25:43 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const { URL } = require('url');
+const path = require('path');
+const stringList = ["Jamie", "Phat", "John", "Arlus", "Osha", "Nonnie", "Adam", "Kit", "Dallin", "Dylan", "Mac", "Ammon", "Calvin", "Dylgar"]
+
+let students = fs.readFileSync(path.join(__dirname, 'students.json'), 'utf8');
+
+students = students.split(',')
+let lastStudent = students[students.length-1]
+students[students.length - 1] = lastStudent.slice(0, lastStudent.length);
+
+let randomNumber = Math.floor(Math.random() * students.length)
+
+let finalString = `+++\n++\n+\n++\n+++\n+++++       ${students.splice(randomNumber, 1).join()}\n+++\n++\n+\n++\n+++`
+
+console.log(finalString);
+
+if (students.length <= 1){
+  students = stringList;
+}
+
+fs.writeFile(path.join(__dirname, 'students.json'), students, ()=>"Success");
