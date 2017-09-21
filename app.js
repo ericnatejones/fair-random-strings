cb07eeaecd1be57487752e0787b678c834e6eda3
Eric Jonese <ericnatejones@gmail.com>
2017-09-21 12:24:04 -0600

    Cleaned up a few conventional things

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 const fs = require('fs');
 const path = require('path');
-const stringList = ["Jamie", "Phat", "John", "Arlus", "Osha", "Nonnie", "Adam", "Kit", "Dallin", "Dylan", "Mac", "Ammon", "Calvin", "Dylgar"]
+const stringList = ["Jamie", "Phat", "John", "Arlus", "Osha", "Nonnie", "Adam", "Kit", "Dallin", "Dylan", "Mac", "Ammon", "Calvin", "Dylgar"];
 
 let students = JSON.parse(fs.readFileSync(path.join(__dirname, 'students.json'), 'utf8'));
 
-let randomNumber = Math.floor(Math.random() * students.length)
+let randomNumber = Math.floor(Math.random() * students.length);
 
-let finalString = `+++\n++\n+\n++\n+++\n+++++       ${students.splice(randomNumber, 1).join()}\n+++\n++\n+\n++\n+++`
-
-console.log(finalString);
+let finalString = `+++\n++\n+\n++\n+++\n+++++          ${students.splice(randomNumber, 1).join()}\n+++\n++\n+\n++\n+++`;
 
 if (students.length === 0){
   students = stringList;
 }
 
-fs.writeFile(path.join(__dirname, 'students.json'), JSON.stringify(students), ()=>"Success");
+fs.writeFile(path.join(__dirname, 'students.json'), JSON.stringify(students), ()=>{console.log(finalString)});
